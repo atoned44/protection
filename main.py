ef1a3a1f43f8e622b0bc30d9a62b44f661849da4
Anike A <68375146+atoned44@users.noreply.github.com>
2023-07-08 01:32:12 +0100

    Update main.py
    changed the scheduling API to APScheduler

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger
 import os
 import requests
-import schedule
-import time
 from datetime import datetime
 from pytz import timezone
+import logging
 
 # Set the Pushsafer API URL and access key
 PUSHSAFER_URL = "https://www.pushsafer.com/api"
@@ -12,37 +13,43 @@ pushsafer_access_key = os.environ.get("PUSHSAFER_ACCESS_KEY")
 # Set the timezone for London
 LONDON_TZ = timezone("Europe/London")
 
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 def send_notification():
     # Get the current time in the London timezone
     now = datetime.now(LONDON_TZ)
     hour = now.hour
-    if 8 <= hour < 18:
 
-        # Set the URL of your static webpage with the message
-        message_url = "https://atoned44.github.io/protection/"
-        
-        # Set the notification payload with the link to the message and other parameters
-        payload = {
-            "k": pushsafer_access_key,
-            "m": f"Recite this spell at {message_url}",
-            "u": message_url,
-            "s": "1",
-        }
+    # Set the URL of your static webpage with the message
+    message_url = "https://atoned44.github.io/protection/"
+    
+    # Set the notification payload with the link to the message and other parameters
+    payload = {
+        "k": pushsafer_access_key,
+        "m": f"Recite this spell at {message_url}",
+        "u": message_url,
+        "s": "1",
+    }
 
-        # Send the notification via the Pushsafer API
-        try:
-            response = requests.post(PUSHSAFER_URL, data=payload)
-            if response.status_code == 200:
-                print("Notification sent successfully.")
-            else:
-                print(f"Failed to send notification: {response.text}")
-        except Exception as e:
-            print(f"Failed to send notification: {e}")
+    # Send the notification via the Pushsafer API
+    try:
+        response = requests.post(PUSHSAFER_URL, data=payload)
+        if response.status_code == 200:
+            logging.info("Notification sent successfully.")
+        else:
+            logging.error(f"Failed to send notification: {response.text}")
+    except requests.exceptions.Timeout as e:
+        logging.error(f"Failed to send notification due to timeout: {e}. Retrying in 5 minutes.")
+        time.sleep(300)
+        send_notification()
 
-# Schedule the send_notification function to run every 2 hours
-schedule.every(3).hours.do(send_notification)
+# Create a scheduler instance and add a cron trigger for weekdays and weekends
+scheduler = BlockingScheduler(timezone=LONDON_TZ)
+weekday_trigger = CronTrigger(day_of_week='mon-fri', hour='8-18/3')
+weekend_trigger = CronTrigger(day_of_week='sat,sun', hour=9)
+scheduler.add_job(send_notification, weekday_trigger)
+scheduler.add_job(send_notification, weekend_trigger)
 
-# Run the scheduled tasks indefinitely
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+# Start the scheduler
+scheduler.start()
