77d628f8157c546510fe77053e8aba8fb8b4d5dc
AtoneAnnie <68375146+atoned44@users.noreply.github.com>
2023-11-30 08:43:50 +0000

    Update main.py
    Removed link in notification message.

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def send_notification():
     # Set the notification payload with the link to the message and other parameters
     payload = {
         "k": pushsafer_access_key,
-        "m": f"Recite your incantations, spells & mantras @ {message_url}",
+        "m": f"Recite your incantations, spells & mantras.",
         "u": message_url,
         "s": "1",
     }
